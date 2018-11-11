f44456caf361700a74b4339f8850766f400c5f5f
วิภาดา มีสกุล <iamfinemook@hotmail.com>
2018-11-11 19:24:09 +0700

    COmmit

---
+++ Shark_Game/Main.cpp
@@ -0,0 +1,25 @@
+#include<SFML/Graphics.hpp> 
+#include<iostream>
+
+using namespace sf;
+using namespace std;
+int main()
+{
+	RenderWindow window(VideoMode(1280,1024),"Shark Game",Style::Default);
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case Event::Closed:
+				window.close();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	return 0;
+}
